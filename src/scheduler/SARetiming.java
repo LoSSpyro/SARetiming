818df4a97eabb3d900c9322dbbb207e3e91580bb
Simon Vetter <simetist@gmail.com>
2019-01-18 15:51:47 +0100

    implemented random retiming move

--- src/scheduler/SARetiming.java
+++ src/scheduler/SARetiming.java
@@ -1,7 +1,12 @@
 package scheduler;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
+
+import javafx.scene.control.RadioMenuItem;
 import javafx.util.Pair;
 
 public class SARetiming {
@@ -23,18 +28,71 @@ public class SARetiming {
 	
 	
 	private boolean doRandomMove() {
-		// TODO
-		return false;
+		List<Pair<Node, Integer>> possibleMoves = getPossibleMoves();
+		int moveIndex = new Random().nextInt(possibleMoves.size());
+		Pair<Node, Integer> move = possibleMoves.get(moveIndex);
+		
+		return retimeNode(move);
 	}
 	
-	private Map<Node, Pair<Integer, Integer>> possibleMoves() {
-		// TODO
-		return new HashMap<Node, Pair<Integer, Integer>>();
+	private List<Pair<Node, Integer>> getPossibleMoves() {
+		List<Pair<Node, Integer>> result = new ArrayList<Pair<Node, Integer>>();
+		
+		for (Node node : graph) {
+			int minIn = Integer.MAX_VALUE;
+			for (Integer weight : node.allPredecessors().values()) {
+				minIn = Math.min(minIn, weight);
+			}
+			int minOut = Integer.MAX_VALUE;
+			for (Integer weight : node.allSuccessors().values()) {
+				minOut = Math.min(minOut, weight);
+			}
+			
+			for (int iterShift = -minIn; iterShift <= minOut; iterShift++) {
+				result.add(new Pair<Node, Integer>(node, new Integer(iterShift)));				
+			}
+		}
+		
+		return result;
 	}
 	
+	private boolean retimeNode(Pair<Node, Integer> move) {
+		return retimeNode(move.getKey(), move.getValue());
+	}
 	private boolean retimeNode(Node node, int iterationShift) {
-		// TODO
-		return false;
+		if (!isMoveValid(node, iterationShift)) {
+			return false;
+		}
+		
+		for (Node predecessor : node.allPredecessors().keySet()) {
+			Integer newWeight = node.allPredecessors().get(predecessor).intValue() + iterationShift;
+			node.allPredecessors().put(predecessor, newWeight);
+			predecessor.allSuccessors().put(node, newWeight);
+		}
+		for (Node successor : node.allSuccessors().keySet()) {
+			Integer newWeight = node.allSuccessors().get(successor).intValue() - iterationShift;
+			node.allSuccessors().put(successor, newWeight);
+			successor.allPredecessors().put(node, newWeight);
+		}
+		
+		return true;
+	}
+	
+	private boolean isMoveValid(Node node, int iterationShift) {
+		for (Node predecessor : node.allPredecessors().keySet()) {
+			int newWeight = node.allPredecessors().get(predecessor).intValue() + iterationShift;
+			if (newWeight < 0) {
+				return false;
+			}
+		}
+		for (Node successor : node.allSuccessors().keySet()) {
+			int newWeight = node.allSuccessors().get(successor).intValue() - iterationShift;
+			if (newWeight < 0) {
+				return false;
+			}
+		}
+		
+		return true;
 	}
 	
 	private boolean exitCriterion() {
